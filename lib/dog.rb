178c1e0005d1b0a980da12077fa0b4e469a53ce5
ROBERT  ALEDO <robertaledo22@gmail.com>
2020-04-09 05:19:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,19 @@
  
 class Dog
-  
-	attr_reader :name
-	attr_accessor :mood 
+  # code goes here
+  attr_accessor :mood, :owner
+  attr_reader :name
 
-	def initialize(name)
-		@name = name 
-		@mood = "nervous"
-	end
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @mood = "nervous"
+    @owner = owner
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
